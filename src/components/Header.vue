3e6c69a911074a318d665ba22cc64087bd31c399
Manish Raj Silwal <manishraj@abhiyantrik.com>
2020-07-29 18:00:08 +0545

    Item moved into the third place in the header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,8 +8,8 @@
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
         <b-nav-item to="/dashboard">Dashboard</b-nav-item>
-        <b-nav-item to="/encountermodify">Flag</b-nav-item>
         <b-nav-item to="/treatment">Treatment Data</b-nav-item>
+        <b-nav-item to="/encountermodify">Flag</b-nav-item>
         <b-nav-item to="/users">Users</b-nav-item>
         <b-nav-item to="/geography">Area</b-nav-item>
         <b-nav-item-dropdown>
